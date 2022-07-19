4761405d01f7d435c725a232ee771935bd81c4c5
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2022-07-19 23:11:17 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -34,7 +34,6 @@ function showSuperAttackAnimations () {
         `)
     game.resume()
     basic.pause(500)
-    isSuperAttackIsRunning = false
 }
 function moveEnemy (sprite: game.LedSprite) {
     for (let index = 0; index < 4; index++) {
@@ -43,13 +42,16 @@ function moveEnemy (sprite: game.LedSprite) {
     }
 }
 function shoot () {
+    serial.writeLine("In function shoot()")
     bullet = game.createSprite(player.get(LedSpriteProperty.X), player.get(LedSpriteProperty.Y))
-    music.playMelody("C5 B A G F F F F ", 5000)
+    // music.playMelody("C5 B A G F F F F ", 5000)
     for (let index = 0; index < 4; index++) {
         bullet.change(LedSpriteProperty.Y, -1)
-        bullet.setBrightness(80)
-basic.pause(100)
+        // bullet.setBrightness(80)
+        serial.writeLine("Is bullet deletet: " + bullet.isDeleted())
+        basic.pause(100)
     }
+    shootColdown = false
 }
 function superAttack () {
     if (bullet == null) {
@@ -60,15 +62,26 @@ function superAttack () {
     }
     bullet.delete()
     enemy.delete()
+    shootColdown = false
 }
 // Input for Button 'AB'
 input.onButtonPressed(Button.AB, function () {
+    // fehler in shoot()
+    serial.writeLine("" + (shootColdown))
     if (input.buttonIsPressed(Button.AB)) {
+        if (shootColdown == true) {
+            return
+        }
         superAttack()
         showSuperAttackAnimations()
+        shootColdown = true
         return
     } else {
+        if (shootColdown == true) {
+            return
+        }
         shoot()
+        shootColdown = true
     }
 })
 // Input for Button 'B'
@@ -86,31 +99,14 @@ function setEnemyPosition (randomNumber: number) {
     moveEnemy(enemy)
 }
 let randomNumberForEnemyPosition = 0
-let shootColdown = false
 let enemy: game.LedSprite = null
-let isSuperAttackIsRunning = false
-let player: game.LedSprite = null
+let shootColdown = false
 let bullet: game.LedSprite = null
+let player: game.LedSprite = null
 // Create Player
 player = game.createSprite(2, 4)
 // Log
 serial.writeLine("Start Log for Game 'Space Shoother'")
-basic.forever(function () {
-    if (bullet == null) {
-        return
-    }
-    if (bullet.get(LedSpriteProperty.Y) == 0) {
-        bullet.delete()
-    }
-})
-basic.forever(function () {
-    if (isSuperAttackIsRunning == false) {
-        return
-    }
-    if (!(bullet == null)) {
-        bullet.delete()
-    }
-})
 basic.forever(function () {
     if (enemy == null) {
         return
@@ -137,4 +133,17 @@ basic.forever(function () {
 basic.forever(function () {
     randomNumberForEnemyPosition = randint(0, 4)
     setEnemyPosition(randomNumberForEnemyPosition)
+})
+basic.forever(function () {
+    if (bullet == null) {
+        return
+    }
+    if (bullet.get(LedSpriteProperty.Y) == 0) {
+        bullet.delete()
+    }
+})
+basic.forever(function () {
+    if (!(bullet == null)) {
+        bullet.delete()
+    }
 })
